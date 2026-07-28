b270edd32e64d8f5f5d7a602a590bba7626854ca
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2026-07-28 04:35:41 +0330

    Random user-agents module & black
    Co-Authored-By: MohammadHosseinTouri <247360775+MHTOURI@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# -- INITAL --
+from utils.user_agents import get_random_user_agent
+
+
+headers = {
+    "User-Agent": get_random_user_agent(
+        # os="windows",
+        # browser="chrome",
+    )
+}
+
+print(headers)
